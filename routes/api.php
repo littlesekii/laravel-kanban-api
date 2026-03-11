87c453a94038fb8d7d918c4452d9c586847ac769
littlesekii <contact.sekii@gmail.com>
2026-03-11 16:48:57 -0300

    project: start project

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\v1\MainController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', [MainController::class, 'index']);
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
